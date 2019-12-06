d0af632e873d2621966078a1fc8c52003a01008a
renzhiwei <729079155@qq.com>
2019-12-06 11:27:43 +0800

    第七节完成

---
+++ demo01/src/xiaojiejie.js
@@ -0,0 +1,21 @@
+import React, { Component,Fragment } from 'react';
+ 
+export class xiaojiejie extends Component {
+    render() {
+        return (
+            <Fragment>
+                <div>
+                    <input/>
+                    <button>增加服务</button>
+                </div>
+                <ul>
+                    <li>服务1</li>
+                    <li>服务2</li>
+                    <li>服务3</li>
+                </ul>
+            </Fragment>
+        );
+    }
+}
+
+export default xiaojiejie;
