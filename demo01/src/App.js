7c0aaea90d6cecd709ee7ee7369cdd344b8c688a
renzhiwei <729079155@qq.com>
2019-12-06 11:12:00 +0800

    减少嵌套

---
+++ demo01/src/App.js
@@ -0,0 +1,13 @@
+import React, {
+    Component
+} from "react";
+class App extends Component {
+    render() {
+        return ( <div >
+            nihao, react. 
+            {false?"成立":"不成立"}
+            </div>
+        )
+    }
+};
+export default App;
