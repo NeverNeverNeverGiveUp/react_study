d34b76aa9fe4848d69a9789b59154a08c86d7192
renzhiwei <729079155@qq.com>
2019-12-11 10:48:16 +0800

    redux 4完成

--- demo02/src/TodoList.js
+++ demo02/src/TodoList.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import "antd/dist/antd.css";
-import { Input } from 'antd'
+import { Input,Button,List } from 'antd'
 
+const data=[
+    '早8点开晨会，分配今天的开发工作',
+    '早9点和项目经理作开发需求讨论会',
+    '晚5:30对今日代码进行review'
+];
 class TodoList extends Component {
     constructor(props) {
         super(props);
@@ -10,7 +15,23 @@ class TodoList extends Component {
     render() {
         return (
             <div>
-                <Input placeholder="renzhiwei" style={{width:'200px'}}></Input>
+                <div>
+                    <Input placeholder="renzhiwei" style={{width:'200px',marginRight:"10px"}}></Input>
+                    <Button type="primary">增加</Button>
+                </div>
+                <div style={{marginTop:"10px",width:"300px"}}>
+                    <List
+                    bordered
+                    dataSource={data}
+                    renderItem={item => (
+                        <List.Item>
+                           {item}
+                        </List.Item>
+                      )}
+                    />
+                    
+                
+                </div>
             </div>
         );
     }
