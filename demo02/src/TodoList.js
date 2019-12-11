5613aad6fcec46ab85cc72c6084a526d573836bd
renzhiwei <729079155@qq.com>
2019-12-11 11:03:36 +0800

    5完成

--- demo02/src/TodoList.js
+++ demo02/src/TodoList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import "antd/dist/antd.css";
-import { Input,Button,List } from 'antd'
+import { Input,Button,List } from 'antd';
+import store from "./store/index";
 
 const data=[
     '早8点开晨会，分配今天的开发工作',
@@ -10,19 +11,20 @@ const data=[
 class TodoList extends Component {
     constructor(props) {
         super(props);
-
+        console.log(store.getState());
+        this.state=store.getState();
     }
     render() {
         return (
             <div>
                 <div>
-                    <Input placeholder="renzhiwei" style={{width:'200px',marginRight:"10px"}}></Input>
+                    <Input placeholder={this.state.inputValue} style={{width:'200px',marginRight:"10px"}}></Input>
                     <Button type="primary">增加</Button>
                 </div>
                 <div style={{marginTop:"10px",width:"300px"}}>
                     <List
                     bordered
-                    dataSource={data}
+                    dataSource={this.state.list}
                     renderItem={item => (
                         <List.Item>
                            {item}
